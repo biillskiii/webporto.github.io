afcdc969657bf4fb58a624dc5a6c7dc9bab83a36
biillskiii <muhamad.nabil20052003@gmail.com>
2023-01-25 22:00:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+  strings: ["Mohammad Nabiel Dwi Ananda","UI/UX Designer", "Video Editor", "Web Designer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
